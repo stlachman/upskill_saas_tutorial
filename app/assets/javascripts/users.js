c0f4a1280b7d4cf6a8a1942e661f077c1601dafb
Seth Lachman <lachmanseth17@gmail.com>
2016-12-12 02:24:48 +0000

    Added stripe response handler and error handling

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -8,16 +8,52 @@ $(document).on('turbolinks:load', function(){
     
     submitBtn.click(function(event){
         event.preventDefault();
+        submitBtn.val("Processing").prop('disabled', true);
         var ccNum = $('#card_number').val(),
             cvcNum = $('#card_code').val(),
             expMonth = $('#card_month').val(),
             expYear = $('#card_year').val();
             
-       Stripe.createToken({
-           number: ccNum,
-           cvc: cvcNum,
-           month: expMonth,
-           year: expYear
-       }, stripeResponseHandler);
-    })
+        // Use stripe library to take care of errors
+        
+        var error = false;
+        
+        if(!Stripe.card.validateCardNumber(ccNum)) {
+            error = true;
+            alert('The credit card number appears to be invalid.')
+        }
+        
+        if(!Stripe.card.validateCVC(cvcNum)) {
+            error = true;
+            alert('The credit card CVC number appears to be invalid.')
+        }
+        
+        if(!Stripe.card.validateExpiry(expMonth, expYear)) {
+            error = true;
+            alert('The expiration date appears to be invalid.')
+        }
+        
+        if (error) {
+            submitBtn.prop('disabled', false).val("Sign Up");
+        } else {
+               Stripe.createToken({
+               number: ccNum,
+               cvc: cvcNum,
+               month: expMonth,
+               year: expYear
+           }, stripeResponseHandler);
+        }
+            
+      
+       return false;
+    });
+    
+    function stripeResponseHandler(status, response){
+        // Get the token from response
+        var token = response.id;
+        // Inject the card token
+        theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );  
+        
+        theForm.get(0).submit();
+    }
 });
